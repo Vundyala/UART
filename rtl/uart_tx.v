3f66787507347fc0a5e3c572afc143a23f70c000
AmanagantiTejasri <amanagantitejasri7974@gmail.com>
2026-08-08 20:29:39 +0530

    Update uart_tx.v

--- rtl/uart_tx.v
+++ rtl/uart_tx.v
@@ -9,31 +9,38 @@ module uart_tx(
     output reg  parity_tx 
 ); 
     reg [3:0] bit_index; 
-    reg [11:0] shift_reg; 
-    always @(posedge clk or posedge reset) begin 
-        if (reset) begin 
-            tx <= 1'b1; 
-            tx_busy <= 0; 
-            bit_index <= 0; 
-            shift_reg <= 12'b11111111111; 
-            parity_tx <= 0; 
-        end else begin 
-            if (tx_start && !tx_busy) begin 
-                parity_tx <=~(^tx_data); 
-                shift_reg <= {2'b11, ~(^tx_data), tx_data, 1'b0}; 
-                tx_busy <= 1; 
-                bit_index <= 0; 
-                tx<=1'b0; 
-            end 
-            else if (baud_tick && tx_busy) begin 
-                tx <= shift_reg[0]; 
-                shift_reg <= {2'b11, shift_reg[10:1]}; 
-                bit_index <= bit_index + 1; 
- 
-                if (bit_index == 11) begin 
-                    tx_busy <= 0; 
-                end 
-            end 
-        end 
-    end 
+    reg [10:0] shift_reg; 
+    always @(posedge clk or posedge reset)
+          begin 
+          if (reset) 
+              begin 
+              tx <= 1'b1; 
+              tx_busy <= 0; 
+              bit_index <= 0; 
+              shift_reg <= 11'b111111111111;                                   //Frame shift register
+              parity_tx <= 0; 
+              end 
+           else
+               begin 
+               if (tx_start && !tx_busy) 
+                   begin 
+                   parity_tx <=~(^tx_data);                                   //odd parity 
+                   shift_reg <= {2'b1, ~(^tx_data), tx_data, 1'b0}; 
+                   tx_busy <= 1; 
+                   bit_index <= 0; 
+                   tx<=1'b0; 
+                   end 
+               else 
+                   begin
+                   if (baud_tick && tx_busy) 
+                       begin 
+                       tx <= shift_reg[0]; 
+                       shift_reg <= {2'b11, shift_reg[10:1]}; 
+                       bit_index <= bit_index + 1;
+                       if (bit_index == 11)
+                           tx_busy <= 0; 
+                       end 
+                  end
+               end
+          end
 endmodule 
