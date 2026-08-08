7f9e8e48c39e14b08f3ed8a6ac2950228679f987
AmanagantiTejasri <amanagantitejasri7974@gmail.com>
2026-08-08 23:10:24 +0530

    Update uart_rx.v

--- rtl/uart_rx.v
+++ rtl/uart_rx.v
@@ -7,84 +7,103 @@ module uart_rx_16x(
     output reg  rx_done, 
     output reg parity_rx 
 ); 
+    //FSM state definition
     local parameter IDLE=3'b000,
     local parameter START=3'b001,
     local parameter DATA=3'b010, 
     local parameter PARITY=3'b011,
     local parameter STOP=3'b100;  
+    
     reg [2:0] state; 
     reg [3:0] sample_cnt; 
     reg [2:0] bit_index; 
     reg [7:0] data_reg; 
     reg rx_parity_bit; 
+    
     always @(posedge clk or posedge reset) begin 
-        if (reset) begin 
+        if (reset) 
+            begin 
             state <= IDLE; 
             sample_cnt <= 0; 
             bit_index <= 0; 
-             data_reg<= 0; 
+            data_reg<= 0; 
             rx_data <= 0; 
             rx_done <= 0; 
             parity_rx <= 0; 
             rx_parity_bit <= 0; 
         end  
-        else if (tick_16x) begin 
-            rx_done <= 0; 
-            case(state) 
-            IDLE: 
+        else 
+            if (tick_16x) 
                 begin 
-                if (rx == 0) begin 
-                    state <= START; 
-                    sample_cnt <= 0; 
-                end 
-            end 
- 
-            START: begin 
-                if (sample_cnt == 15) begin 
-                    state <= DATA; 
-                    bit_index <= 0; 
-                    sample_cnt <= 0; 
-                end else 
-                    sample_cnt <= sample_cnt + 1; 
-            end 
- 
-            DATA: begin 
-                if (sample_cnt == 15) begin 
-                    data_reg[bit_index] <= rx; 
-                    sample_cnt <= 0; 
-                    if (bit_index == 7) 
-                        state <= PARITY; 
-                    else 
-                        bit_index <= bit_index + 1; 
-                end else 
-                    sample_cnt <= sample_cnt + 1; 
-            end 
- 
-            PARITY: begin 
-                if (sample_cnt == 15) begin 
-                    rx_parity_bit <= rx; 
-                    parity_rx <= ~(^data_reg); 
-                    if (rx_parity_bit==parity_rx) 
-                        state <= STOP; 
-                    else 
-                        state <= IDLE; 
-                    sample_cnt <= 0; 
-                end else 
-                    sample_cnt <= sample_cnt + 1; 
-            end 
- 
-            STOP: begin 
-                if (sample_cnt == 15) begin 
-                    if (rx == 1) begin 
-                        rx_data <= data_reg; 
-                        rx_done <= 1; 
+                rx_done <= 0; 
+                case(state) 
+              IDLE: 
+                    begin 
+                    if (rx == 0) 
+                        begin                                           
+                        state <= START; 
+                        sample_cnt <= 0; 
+                        end 
                     end 
-                    state <= IDLE; 
-                    sample_cnt <= 0; 
-                end else 
-                    sample_cnt <= sample_cnt + 1; 
-            end 
  
+               START:
+                   begin 
+                       if (sample_cnt == 15) 
+                       begin 
+                       state <= DATA;                                   
+                       bit_index <= 0; 
+                       sample_cnt <= 0; 
+                       end 
+                   else 
+                       sample_cnt <= sample_cnt + 1; 
+                    end 
+ 
+                DATA: 
+                    begin 
+                    if (sample_cnt == 15) 
+                        begin 
+                        data_reg[bit_index] <= rx;                 //store sampled bit
+                        sample_cnt <= 0; 
+                            if (bit_index == 7)                    //All bits are recieved , move to parity state
+                            state <= PARITY; 
+                        else 
+                           bit_index <= bit_index + 1; 
+                        end
+                    else 
+                       sample_cnt <= sample_cnt + 1; 
+                    end 
+ 
+                PARITY: 
+                    begin 
+                    if (sample_cnt == 7)
+                        begin 
+                        rx_parity_bit <= rx; 
+                        parity_rx <= ~(^data_reg);                //odd parity
+                        if (rx_parity_bit==parity_rx) 
+                            state <= STOP;                        // Recieved parity matches calculated parity,move to state STOP
+                        else 
+                           state <= IDLE; 
+                           sample_cnt <= 0;                       //Parity mismatch,move to IDLE state
+                    end 
+                    else 
+                       sample_cnt <= sample_cnt + 1; 
+                    end 
+ 
+                 STOP:
+                     begin 
+                         if (sample_cnt == 15) 
+                         begin 
+                         if (rx == 1) 
+                             begin 
+                             rx_data <= data_reg; 
+                             rx_done <= 1; 
+                             end 
+                        state <= IDLE; 
+                        sample_cnt <= 0; 
+                         end 
+                      else 
+                      sample_cnt <= sample_cnt + 1; 
+                    end 
             endcase 
         end 
     end 
